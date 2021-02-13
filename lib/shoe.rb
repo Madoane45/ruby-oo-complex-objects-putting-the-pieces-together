6d22d715f44a8a1963d57d59037e492c65a74658
Madoane45 <Madoane45@gmail.com>
2021-02-13 14:25:42 -0500

    commit tree 5314127b2ebb1eb5e58d9ccf3546e0f201746525

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    
+    def initialize(brand)
+        @brand = brand
+        @condition = condition
+       
+    end 
+    def wear
+        self.condition = "worn"
+    end
+    def cobble
+        self.condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+        #def report
+            #if self.condition == "new"
+            #puts "Your shoe is as good as new!"
+           
+           # end
+        #end
+end
